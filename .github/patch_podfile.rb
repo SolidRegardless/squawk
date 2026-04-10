116b0315acb85fd86d88c79c06018246613d84c6
Stuart Hart <snhcomputingltd@gmail.com>
2026-04-10 11:46:37 +0100

    fix: inject signing patch into existing post_install hook, not append new one

--- .github/patch_podfile.rb
+++ .github/patch_podfile.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-# Appends a post_install hook to the Podfile that disables code signing
-# on all CocoaPods targets. Run from client/ios/App directory.
+# Injects CODE_SIGNING_ALLOWED=NO into the existing post_install hook in the Podfile.
+# Capacitor generates a post_install hook already; we can't add a second one.
 
 podfile_path = 'Podfile'
 content = File.read(podfile_path)
@@ -10,18 +10,27 @@ if content.include?('CODE_SIGNING_ALLOWED')
   exit 0
 end
 
-patch = <<~RUBY
-
-  post_install do |installer|
-    installer.pods_project.targets.each do |target|
-      target.build_configurations.each do |config|
-        config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
-        config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
-        config.build_settings['CODE_SIGN_STYLE'] = 'Automatic'
+injection = <<~RUBY
+      # CI: disable signing on Pod targets so only App target uses the provisioning profile
+      installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+          config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
+          config.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
+          config.build_settings['CODE_SIGN_STYLE'] = 'Automatic'
+        end
       end
-    end
-  end
 RUBY
 
-File.open(podfile_path, 'a') { |f| f.write(patch) }
-puts 'Podfile patched'
+# Insert our code at the start of the existing post_install block
+patched = content.sub(
+  /^(post_install do \|installer\|)/,
+  "\\1\n#{injection}"
+)
+
+if patched == content
+  warn 'ERROR: Could not find post_install hook to patch'
+  exit 1
+end
+
+File.write(podfile_path, patched)
+puts 'Podfile patched successfully'
